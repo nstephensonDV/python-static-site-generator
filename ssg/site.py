5cca14527ff7503ab7d1466728e508a86dd09c86
Nick Stephenson <nstephenson@dixonvalve.com>
2022-10-17 17:00:29 -0400

    copy of main

--- ssg/site.py
+++ ssg/site.py
@@ -1,9 +1,7 @@
 from pathlib import Path
 
-#Adding a comment line
 
 class Site:
-
     def __init__(self, source, dest):
         self.source = Path(source)
         self.dest = Path(dest)
@@ -17,4 +15,3 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
-
