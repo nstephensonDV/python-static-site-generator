23271f68d2c4aaf3ec75330846d37e10c0660225
Nick Stephenson <nstephenson@dixonvalve.com>
2022-10-17 16:35:58 -0400

    Completed Module

---
+++ ssg.py
@@ -0,0 +1,10 @@
+import typer
+from ssg.site import Site
+
+def main(source = "content", dest = "dist"):
+    config = {"source" : source, "dist" : dest}
+    site = Site(**config)
+    site.build
+
+
+typer.run(main)
