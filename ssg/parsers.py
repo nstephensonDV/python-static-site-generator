4f07162909d43e5864d244110b20c6e208497f62
Nick Stephenson <nstephenson@dixonvalve.com>
2022-10-26 09:35:47 -0400

    Module 2

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -3,34 +3,28 @@ from pathlib import Path
 import shutil
 
 class Parser:
-    def __init__(self, extensions: List[str] = []):
-        self.extensions = extensions
+    extensions: List[str] = []
 
     def valid_extension(self, extension):
-        for i in self.extensions:
-            if extension == i:
-                return True
-        return False
+        return extension in self.extensions
 
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
 
     def read(self, path):
-        with open(path, 'rt', encoding='UTF-8') as file:
+        with open(path, 'r') as file:
             return file.read()
 
-    def write(self, path: Path, dest: Path, content, ext = ".html"):
+    def write(self, path: Path, dest: Path, content, ext=".html"):
         full_path = dest / path.with_suffix(ext).name
-        with open(full_path, 'rt', encoding='UTF-8') as file:
+        with open(full_path, 'w') as file:
             file.write(content)
 
     def copy(self, path: Path, source: Path, dest: Path):
-        shutil.copy2(path, dest / source)
+        shutil.copy2(path, dest / path.relative_to(source))
 
 class ResourceParser(Parser):
-    def __init__(self):
-        extensions = [".jpg", ".png", ".gif", ".css", ".html"]
-        super().__init__(extensions)
+    extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
     def parse(self, path: Path, source: Path, dest: Path):
         super().copy(path, source, dest)
